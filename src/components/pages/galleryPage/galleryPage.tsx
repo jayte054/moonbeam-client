9d76f2f46abf828ce8ea026fa898498948859721
Justin Ewelike <54476858+jayte054@users.noreply.github.com>
2023-12-03 15:10:46 +0100

    gallery page update

--- src/components/pages/galleryPage/galleryPage.tsx
+++ src/components/pages/galleryPage/galleryPage.tsx
@@ -1,9 +1,23 @@
+import { useContext, useEffect, useState } from "react";
 import { BsBasket2Fill } from "react-icons/bs";
+import { GalleryContext } from "../../../context/galleryContext/galleryContext";
 import { Footer } from "../../footer/footer"
 import { GalleryPageNavbar } from "../../navbar/galleryPageNavbar"
 import "./galleryPage.css"
 
 export const GalleryPage = () => {
+    const [products, setProducts] = useState<any[]>([])
+    const {galleryMap} = useContext<any>(GalleryContext)
+
+    useEffect(() => {
+        const getProducts = () => {
+            return (
+                setProducts(galleryMap)
+            )
+        }
+        console.log(products)
+        getProducts()
+    }, [galleryMap, products])
 
     return (
         <div className="quickOrder-container">
@@ -17,11 +31,50 @@ export const GalleryPage = () => {
                 <div className="category-title">
                 <h3>Birthday Cakes</h3>
                 </div>
+                <div className="category-type">
+                {products.map((product: any) => (
+                        <div key={product.productId} >
+                            {product.type === "birthday" ? (
+                                <div className="category-item">
+                                    <img src={product.imageUrl} alt={product.title} /><br />
+                                    <span>{product.description}</span><br />
+                                    <button type= "button">Add to Cart</button>
+                                </div>
+                            ) : null}
+                        </div>
+                    ))}
+                </div>
                 <div className="category-title">
                 <h3>Anniversary Cakes</h3>
                 </div>
+                <div className="category-type">
+                    {products.map((product: any) => (
+                        <div key={product.productId}>
+                            {product.type === "anniversary" ? (
+                                <div className="category-item">
+                                    <img src={product.imageUrl} alt={product.description} /><br />
+                                    <span>{product.description}</span><br />
+                                    <button type= "button">Add to Cart</button>
+                                </div>
+                            ): null}
+                        </div>
+                    ))}
+                </div>
                 <div className="category-title">
                 <h3>Wedding Cakes</h3>
+                </div>
+                <div className="category-type">
+                    {products.map((product: any) => (
+                        <div key={product.productId}>
+                            {product.type === "wedding" ? (
+                                <div className="category-item">
+                                    <img src={product.imageUrl} alt={product.description} /><br />
+                                    <span>{product.description}</span><br />
+                                    <button type= "button">Add to Cart</button>
+                                </div>
+                            ): null}
+                        </div>
+                    ))}
                 </div>
                 <div className="category-title">
                 <h3>Chops & Pastries</h3>
